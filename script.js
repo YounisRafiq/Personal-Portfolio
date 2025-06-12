5650f05f0cbe86082f87653ef8cb4e6e9d7f6954
Younis Rafiq <ya945055@gmail.com>
2025-06-12 03:34:20 -0700

    changed

--- script.js
+++ script.js
@@ -38,5 +38,6 @@ const darkMode = () => {
     else{
         mode = "light"
         document.querySelector("body").classList.remove("darkMode");
+        prof.style.backgroundColor = "white"
     }
 }
