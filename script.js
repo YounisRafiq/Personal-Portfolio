56c4357b889f648a200051afb93dd121facf5618
Younis Rafiq <ya945055@gmail.com>
2025-06-12 02:51:49 -0700

    changed

--- script.js
+++ script.js
@@ -32,8 +32,7 @@ const darkMode = () => {
        let head = document.querySelector(".proffesion h3");
        head.style.color = "white";
        document.querySelector(".proffesion i").style.color = "white";
-       document.querySelectorAll(".services-box h3").style.color = "black"
-       document.querySelectorAll(".services-box p").style.color = "black"
+       document.querySelectorAll(".services-box").style.backgroundColor = "black"
        
     }
     else{
